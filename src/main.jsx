025cf87d866d8eba33032374682274c90c648e1d
Ultra Studios <nathanaelmarcelloh@gmail.com>
2026-04-04 10:15:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './index.css';
 
 const rootElement = document.getElementById('root');
 
