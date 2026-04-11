e628c50d17004bc42640fe6a40bdd1cdfb653f8b
Ultra Studios <nathanaelmarcelloh@gmail.com>
2026-04-11 23:10:37 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
-@type {import('tailwindcss').Config}
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        display: ['"Playfair Display"', 'serif'], 
-        sans: ['Inter', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
