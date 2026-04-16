7c473974e5aeb6378607b65646d84e90fc43f548
Ultra Studios <nathanaelmarcelloh@gmail.com>
2026-04-16 22:00:53 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css' // CRITICAL: Pastikan file Tailwind kamu di-import di sini
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
