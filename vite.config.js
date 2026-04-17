c29e33a064d7c871d6f67ffaf1dfc582a7e283db
Ultra Studios <nathanaelmarcelloh@gmail.com>
2026-04-17 20:22:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
   server: {
-    host: true,
-    strictPort: true
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
